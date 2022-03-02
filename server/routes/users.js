12d432699085de36f5224046c744e0b3ff4b9ec9
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-03-02 11:30:32 +0530

    friend request and unfriend methods added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,8 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const { sendResponse } = require("../app/controllers");
 const User = require("../app/models/Users");
 const bcrypt = require("bcrypt");
+const router = require("./Router");
 
 const ROUTER = new Router();
 
@@ -56,6 +57,47 @@ ROUTER.get("/:id", async (req, res) => {
 });
 
 //follow user
+ROUTER.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const userFriend = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!userFriend.followers.includes(req.body.userId)) {
+        await userFriend.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: req.params.id } });
+        sendResponse(res, "", "User account has been followed", 200, true);
+      } else {
+        res.status(403).json("You already follow this user");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    sendResponse(res, "", "Cannot follow yourself", false, 403);
+  }
+});
+
 //unfollow user
+ROUTER.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const userFriend = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (userFriend.followers.includes(req.body.userId)) {
+        await userFriend.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { following: req.params.id } });
+        sendResponse(res, "", "User account has been unfollowed", 200, true);
+      } else {
+        res.status(403).json("You dont follow this user");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    sendResponse(res, "", "Cannot unfollow yourself", false, 403);
+  }
+});
 
 module.exports = ROUTER;
