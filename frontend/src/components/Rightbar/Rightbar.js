4f7d71405c3626d6941bbd07c4f9db1bac74c131
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-03-05 19:21:09 +0530

    basic homepage outlet completed

--- frontend/src/components/Rightbar/Rightbar.js
+++ frontend/src/components/Rightbar/Rightbar.js
@@ -1,7 +1,34 @@
 import React from "react";
 import "./Rightbar.scss";
+import pacman from "../../images/pacman.jfif";
+import gift from "../../images/gift.jfif";
+import rohit from "../../images/rohit.jfif";
+
 function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImage" src={gift} alt="giftBox" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+
+        <img className="developerPhoto" src={pacman} alt="pacman" />
+        <h4 className="textTitle">Online Friends</h4>
+        <ul className="onlineFriendList">
+          <li className="onlineFriend">
+            <div className="friendContainer">
+              <img className="onlineProfile" src={rohit} alt="" />
+              <span className="online" />
+            </div>
+            <span className="friendUsername">Rohit Sharma</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Rightbar;
